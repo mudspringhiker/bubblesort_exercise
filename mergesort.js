cedcf698e271dcde73dbb94b3d6f2f6865a6e5be
Alona Varshal <thirdwave72@gmail.com>
2018-03-05 18:31:05 -0600

    Fix merge function

--- mergesort.js
+++ mergesort.js
@@ -6,15 +6,26 @@ function split(wholeArray) {
 }
 
 function merge(array1, array2) { // array with two arrays inside
-
+    let subArr = [];
+    while ((array1.length > 0) && (array2.length > 0)) {
+        if (array1[0] < array2[0]) {
+            subArr.push(array1[0]);
+            array1 = array1.slice(1);
+        } else {
+            subArr.push(array2[0])
+            array2 = array2.slice(1);
+        }
+    }
+    subArr = subArr.concat(array1).concat(array2);
+    return subArr;
 }
 
-function mergeSort(arrayToBeSorted) {
-    if (arrayToBeSorted.length <= 1) {
-        return arrayToBeSorted;
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
     } else {
-        let splitArrayFirst = split(arrayToBeSorted)[0];
-        let splitArraySecond = split(arrayToBeSorted)[1];
+        let splitArrayFirst = split(array)[0];
+        let splitArraySecond = split(array)[1];
         return merge(mergeSort(splitArrayFirst), mergeSort(splitArraySecond));      
     }
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -8,6 +8,16 @@ describe('Split Array function', function() {
 
 describe('Merge function', function(){
     it('is able to merge two sorted arrays into one sorted array', function(){
-       expect( merge([[1, 2, 3], [4, 5]])).toEqual([[1, 2, 3, 4, 5]]);
+       expect( merge( [4], [3] ) ).toEqual( [3, 4] );
+    });
+
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      expect( merge( [2, 4], [1, 3] ) ).toEqual( [1, 2, 3, 4] );
+   });
+
+  describe('Mergesort function', function() {
+    it('is able to sort elements in an array', function() {
+      expect( mergeSort([4, 7, 3, 1, 9, 5, 2])).toEqual([1, 2, 3, 4, 5, 7, 9]);
     });
   });
+});
